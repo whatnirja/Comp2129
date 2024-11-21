6690f2256d72ffd470a3885aba68d9fa648e11b1
Danuja-git <danujashankar@gmail.com>
2024-11-21 08:35:26 -0500

    Customer.cs

--- Customer.cs
+++ Customer.cs
@@ -1,41 +1,34 @@
-using System.Threading.Tasks;
-
-namespace _2129_prj
+internal class Customer
 {
-    internal class Customer
+    private int customerId;
+    private string firstName;
+    private string lastName;
+    private string phone;
+    private int numBookings;
+
+    public Customer(int custId, string firstName, string lastName, string phone)
     {
-        public int CustomerID { get; private set; }
-        public string CustomerFname { get; private set; }
-        public string CustomerLname { get; private set; }
-        public string PhoneNumber { get; private set; }
-        public int NumberOfBookings { get; set; }
-
-        // Constructor
-        public Customer(string firstname, string lastname, string phonenumber)
-        {
-            CustomerFname = firstname;
-            CustomerLname = lastname;
-            PhoneNumber = phonenumber;
-            CustomerID = GenerateCustomerID(); // Auto-generate unique ID
-            NumberOfBookings = 0; // Default value
-        }
-
-        // Generate unique ID
-        private static int GenerateCustomerID()
-        {
-            return new Random().Next(1000, 9999);
-        }
-
-        public bool IsValidCustomer()
-        {
-            return !string.IsNullOrEmpty(CustomerFname) &&
-                   !string.IsNullOrEmpty(CustomerLname) &&
-                   !string.IsNullOrEmpty(PhoneNumber);
-        }
-
-        public override string ToString()
-        {
-            return $"ID: {CustomerID}, Name: {CustomerFname} {CustomerLname}, Phone: {PhoneNumber}, Bookings: {NumberOfBookings}";
-        }
+        this.customerId = custId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+        numBookings = 0;
     }
+
+    public int getCustomerID() { return customerId; }
+    public string getFirstName() { return firstName; }
+    public string getLastName() { return lastName; }
+    public string getPhone() { return phone; }
+    public int getNumBookings() { return numBookings; }
+
+    public void setCustomerID(int custID) { this.customerId = custID; }
+    public void setFirstName(string firstName) { this.firstName = firstName; }
+    public void setLastName(string lastName) { this.lastName = lastName; }
+    public void setPhone(string phone) { this.phone = phone; }
+    public override string ToString()
+    {
+        string s = customerId + "\t" + firstName + "\t" + lastName + "\t" + phone;
+        return s;
+    }
+
 }
