d41f6e7dcc6bb15c6e6e050b9d0b76cb3320f20d
gianagpal <gianagpal08@gmail.com>
2024-11-25 04:37:49 -0500

    Coordinator.cs

--- Coordinator.cs
+++ Coordinator.cs
@@ -1,276 +1,312 @@
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
-namespace AirLine
+namespace c2129groupProject
 {
-    public class Coordinator
+    internal class Coordinator
     {
-      private BookingManager bookingManager;
-      private CustomerManager customerManager;
-      private FlightManager flightManager;
+        private CustomerManager customerMan;
+        private FlightManager flightMan;
 
-      public Coordinator()
-      {
-        bookingManager = new BookingManager(); //initializing the booking manager
-        customerManager = new CustomerManager(1000, 0); //initializing the customer manager
-        flightManager = new FlightManager(); //initializing the flight manager
-      }
-
-
-      //booking menuuu
-      public void BookingMenu()
-      {
-        bool exit = false;
-        while(!exit)
+        public Coordinator()
         {
-          Console.WriteLine("ABC Airlines Ltd.");
-          Console.WriteLine("Booking Menu");
-          Console.WriteLine("1. View Bookings");
-          Console.WriteLine("2. Make a Booking");
-          Console.WriteLine("3. Delete a Booking");
-          Console.WriteLine("4. Exit");
-          Console.Write("Enter your choice: ");
-
-          try
-          {
-            string? choice = Console.ReadLine(); //declared as nullable 
-            if (string.IsNullOrWhiteSpace(choice) || !int.TryParse(choice, out int parsedChoice))
-            {
-              Console.WriteLine("Invalid choice, please enter a number.");
-              continue;
-            }
-
-            switch (parsedChoice)
-            {
-              case 1:
-                ViewBookings();
-                break;
-              case 2:
-                AddBooking();
-                break;
-              case 3:
-                DeleteBooking();
-                break;
-              case 4:
-                exit = true;
-                break;
-              default:
-                Console.WriteLine("Invalid choice, please try again.");
-                break;
-            
-            }
-          }
-          catch (Exception ex)
-          {
-            Console.WriteLine($"An error occurred: {ex.Message}");
-          }
-
+            customerMan = new CustomerManager(1000, 1);
+            flightMan=new FlightManager(1000, 400);
         }
-      }
 
-      //flight menu
-      public void FlightMenu()
-      {
-        bool exit = false;
-        while(!exit)
+        //1. Customer
+        public void CustomerMenu()
         {
-          Console.WriteLine("ABC Airlines Ltd.");
-          Console.WriteLine("Flight Menu");
-          Console.WriteLine("1. Add Flight");
-          Console.WriteLine("2. View Flights");
-          Console.WriteLine("3. View a Particular Flight ");
-          Console.WriteLine("4. Delete Flight");
-          Console.WriteLine("5. Exit");
-          Console.Write("Enter your choice: ");
-
-          try
-          {
-            string? choice = Console.ReadLine(); //declared as nullable 
-            if (string.IsNullOrWhiteSpace(choice) || !int.TryParse(choice, out int parsedChoice))
+            bool exit = false;
+            while (!exit)
             {
-              Console.WriteLine("Invalid choice, please enter a number.");
-              continue;
-            }
+                //Customer menu
+                Console.WriteLine("\n---ABC Airlines Ltd.---");
+                Console.WriteLine("Customer Menu");
+                Console.WriteLine();
+                Console.WriteLine("1. Add Customer");
+                Console.WriteLine("2. View Customers");
+                Console.WriteLine("3. Delete Customer");
+                Console.WriteLine("4. Exit");
+                Console.Write("Enter your choice(1-4): ");
 
-            switch (parsedChoice)
-            {
-              case 1:
-                AddFlight();
-                break;
-              case 2:
-                ViewFlights();
-                break;
-              case 3:
-                ViewAParticularFlight();
-                break;
-              case 4:
-                DeleteFlight();
-                break;
-              case 5:
-                exit = true;
-                break;
-              default:
-                Console.WriteLine("Invalid choice, please try again.");
-                break;
-            
-            }
-          }
-          catch (Exception ex)
-          {
-            Console.WriteLine($"An error occurred: {ex.Message}");
-          }
+                try
+                {
+                    string? choice = Console.ReadLine();                       
+                    if (string.IsNullOrWhiteSpace(choice) || !int.TryParse(choice, out int parsedChoice)) 
+                    {
+                        //Loops till valid integer found
+                        Console.Clear();
+                        Console.WriteLine("Invalid choice, please enter integer (1-4).");
+                        continue;
+                    }
 
+                    switch (parsedChoice)
+                    {
+                        case 1:
+                            Console.Clear();
+                            AddCustomer();
+                            break;
+                        case 2:
+                            Console.Clear();
+                            ViewCustomers();
+                            break;
+                        case 3:
+                            Console.Clear();
+                            DeleteCustomer();
+                            break;
+                        case 4:
+                            exit = true;
+                            break;
+                        default:
+                            Console.Clear();
+                            Console.WriteLine("Invalid choice, please enter integer (1-4).");
+                            break;
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"An error occurred: {ex.Message}");
+                }
+
+            }
         }
-      }
 
-      //flight menu
-      public void CustomerMenu()
-      {
-        bool exit = false;
-        while(!exit)
+        //Adds Customer
+        public void AddCustomer()
         {
-          Console.WriteLine("ABC Airlines Ltd.");
-          Console.WriteLine("Customer Menu");
-          Console.WriteLine("1. Add Customer");
-          Console.WriteLine("2. View Customers");
-          Console.WriteLine("3. Delete Customer");
-          Console.WriteLine("4. Exit");
-          Console.Write("Enter your choice: ");
-
-          try
-          {
-            string? choice = Console.ReadLine(); //declared as nullable 
-            if (string.IsNullOrWhiteSpace(choice) || !int.TryParse(choice, out int parsedChoice))
-            {
-              Console.WriteLine("Invalid choice, please enter a number.");
-              continue;
+            bool repeat;
+            repeat = true;
+            while (repeat) { //Repeats until valid input found
+                Console.Clear();
+                Console.WriteLine("Add new Customer");
+                Console.WriteLine("Enter First Name: ");
+                string firstName = Console.ReadLine();
+                Console.WriteLine("Enter Last Name: ");
+                string lastName = Console.ReadLine();
+                Console.WriteLine("Enter Phone Number: ");
+                string phone = Console.ReadLine();
+                if (!string.IsNullOrWhiteSpace(firstName) && !string.IsNullOrWhiteSpace(lastName) && !string.IsNullOrWhiteSpace(phone))
+                {
+                    if (customerMan.AddCustomer(firstName, lastName, phone))
+                    {
+                        Console.WriteLine("New customer added successfuly.");
+                        
+                    }
+                    
+                    repeat = false;
+                }
+                else { Console.WriteLine("Invalid input. All fields must be non-empty."); }
             }
+            
+            
+        }
 
-            switch (parsedChoice)
+        //Viwing all customer data
+        public void ViewCustomers()
+        {
+            customerMan.ViewAllCustomer();
+        }
+
+
+        //Deleting Customer using ID input
+        public void DeleteCustomer()
+        {
+            
+            bool repeat=true;
+
+            while(repeat)
             {
-              case 1:
-                AddCustomer();
-                break;
-              case 2:
                 ViewCustomers();
-                break;
-              case 3:
-                DeleteCustomer();
-                break;
-              case 4:
-                exit = true;
-                break;
-              default:
-                Console.WriteLine("Invalid choice, please try again.");
-                break;
-            
+                Console.WriteLine("\nEnter Customer ID to be deleted(or press e to exit): ");
+                string input=Console.ReadLine();
+                if (input.ToLower() == "e")
+                {
+                    Console.Clear();
+                    break;
+                }
+                if (int.TryParse(input, out int customerId))
+                {
+                    if (customerMan.DeleteCustomer(customerId))
+                    {
+                        
+                        repeat = false;
+                    }
+                    
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Invalid Id. Please try again. ");
+                }
             }
-          }
-          catch (Exception ex)
-          {
-            Console.WriteLine($"An error occurred: {ex.Message}");
-          }
 
+
+
+       
+            
         }
-      }
-
-      //methods for the flight menu 
-      private void AddFlight()
-      {
-        flightManager.AddFlight();
-      }
-
-      private void ViewFlights()
-      {
-        flightManager.ViewFlights();
-      }
-
-      private void ViewAParticularFlight()
-      {
-        Console.WriteLine("Enter Flight ID: ");
-        int flightId = Convert.ToInt32(Console.ReadLine());
-        flightManager.ViewParticularFlight(flightId);
-      }
-
-      private void DeleteFlight()
-      {
-        flightManager.DeleteFlight();
-      }
-    
-
-      //methods for the booking menu
-      private void AddBooking()
-      {
-        Console.WriteLine("Enter Customer ID: ");
-        int customerId = Convert.ToInt32(Console.ReadLine());
-
-        Console.WriteLine("Enter Flight ID: ");
-        int flightId = Convert.ToInt32(Console.ReadLine());
-
-        Customer customer = new CustomerManager(1000, 0).searchCustByID(customerId);
-        Flight flight = flightManager.getFlight(flightId);
 
 
-          
-      if (customer == null)
-      {
-        Console.WriteLine("Customer ID not found :(");
-        return;
-      }
-      if(flight == null)
-      {
-        Console.WriteLine("Flight ID not found :(");
-        return;
-      }
 
-      if(flight.getPassengers() <= flight.getMaxSeats())
-      {
-        Console.WriteLine("No available seats :(");
-        return;
-      }
+        //2. Flights
+        public void FlightMenu()
+        {
+            bool exit = false;
+            while (!exit)
+            {
+                Console.WriteLine("\n---ABC Airlines Ltd.---");
+                Console.WriteLine("Flight Menu");
+                Console.WriteLine();
+                Console.WriteLine("1. Add Flight");
+                Console.WriteLine("2. View Flights");
+                Console.WriteLine("3. View a Particular Flight ");
+                Console.WriteLine("4. Delete Flight");
+                Console.WriteLine("5. Exit");
+                Console.Write("Enter your choice: ");
 
-      bookingManager.MakeBooking(customer, flight);
+                try
+                {
+                    string? choice = Console.ReadLine(); //declared as nullable 
+                    if (string.IsNullOrWhiteSpace(choice) || !int.TryParse(choice, out int parsedChoice))
+                    {
+                        Console.WriteLine("Invalid choice, please enter a number.");
+                        continue;
+                    }
 
-      Console.WriteLine("Booking for Customer with Customer ID " + customerId + " successful.");      
+                    switch (parsedChoice)
+                    {
+                        case 1:
+                            Console.Clear();
+                            AddFlight();
+                            break;
+                        case 2:
+                            Console.Clear();
+                            ViewAllFlights();
+                            break;
+                        case 3:
+                            Console.Clear();
+                            ViewAParticularFlight();
+                            break;
+                        case 4:
+                            Console.Clear();
+                            DeleteFlight();
+                            break;
+                        case 5:
+                            exit = true;
+                            break;
+                        default:
+                            Console.WriteLine("Invalid choice, please try again.");
+                            break;
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"An error occurred: {ex.Message}");
+                }
+
+            }
+        }
+
+        public void AddFlight()
+        {
+            bool repeat;
+            repeat = true;
+            while (repeat)
+            { //Repeats until valid input found
+                Console.Clear();
+                Console.WriteLine("--Add new Flight--");
+                Console.WriteLine("Enter Origin: ");
+                string origin = Console.ReadLine();
+                Console.WriteLine("Enter Destination: ");
+                string destination = Console.ReadLine();
+                Console.WriteLine("Enter maximum seats: ");
+                string maxSeats = Console.ReadLine();
+                if (!string.IsNullOrWhiteSpace(origin) && !string.IsNullOrWhiteSpace(destination) && !string.IsNullOrWhiteSpace(maxSeats))
+                {
+                    if (flightMan.AddFlight(origin, destination, int.Parse(maxSeats)))
+                    {
+                        Console.Clear() ;
+                        Console.WriteLine($"Flight added successfully.");
+
+                    }
+
+                    repeat = false;
+                }
+                else { Console.WriteLine("Invalid input. All fields must be non-empty."); }
+            }
+        }
+        public void ViewAllFlights()
+        {
+            flightMan.ViewFlights();
+        }
+
+        public void ViewAParticularFlight()
+        {
+            bool repeat = true;
+
+            while (repeat)
+            {
+                
+                Console.WriteLine("\nEnter Flight number to be viewed(or press e to exit): ");
+                string input = Console.ReadLine();
+                if (input.ToLower() == "e")
+                {
+                    Console.Clear();
+                    break;
+                }
+                if (int.TryParse(input, out int flightNum))
+                {
+                    flightMan.ViewParticularFlight(flightNum);
+                    repeat = false;
+
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Invalid Id. Please try again. ");
+                }
+            }
+        }
+
+        public void DeleteFlight()
+        {
+            bool repeat = true;
+            
+            while (repeat)
+            {
+                ViewAllFlights();
+                Console.WriteLine("\nEnter Flight number to be deleted(or press e to exit): ");
+                string input = Console.ReadLine();
+                if (input.ToLower() == "e")
+                {
+                    Console.Clear();
+                    break;
+                }
+                if (int.TryParse(input, out int flightNum))
+                {
+                    if (flightMan.DeleteFlight(flightNum))
+                    {
+                        Console.Clear() ;
+                        Console.WriteLine($"Flight with number {flightNum} deleted.");
+                        repeat = false;
+                    }
+
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Invalid Id. Please try again. ");
+                }
+            }
+        }
+
+
     }
-
-    private void ViewBookings()
-    {
-      bookingManager.ViewBookings();
-    }
-
-    private void DeleteBooking()
-    {
-      Console.WriteLine("Enter Booking ID: ");
-      int bookingId = Convert.ToInt32(Console.ReadLine());
-      bookingManager.DeleteBooking(bookingId);
-    }
-
-    //methods for the customer menu
-    private void AddCustomer()
-    {
-      Console.WriteLine("Enter First Name: ");
-      string firstName = Console.ReadLine();
-      Console.WriteLine("Enter Last Name: ");
-      string lastName = Console.ReadLine();
-      Console.WriteLine("Enter Phone Number: ");
-      string phone = Console.ReadLine();
-      customerManager.addCustomer(firstName, lastName, phone);
-    }
-
-    private void ViewCustomers()
-    {
-      customerManager.viewAllCustomers();
-    }
-
-    private void DeleteCustomer()
-    {
-      Console.WriteLine("Enter Customer ID: ");
-      int customerId = Convert.ToInt32(Console.ReadLine());
-      customerManager.deleteCustomer(customerId);
-    }
-
-  }
 }
