a93dcffa44a8b8e24c7755ff598fe62cb8492227
gianagpal <gianagpal08@gmail.com>
2024-11-25 04:39:18 -0500

    Flight.cs

--- Flight.cs
+++ Flight.cs
@@ -1,19 +1,23 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AirLine
+namespace c2129groupProject
 {
-    public class Flight
+    internal class Flight
     {
-        protected int flightNum;
-        protected string origin;
-        protected string destination;
-        protected int maxSeats;
-        protected int passengers;
-        Customer[] customers = new Customer[0];
+        public int flightNum { get; set; }
+        public string origin { get; set; }
+        public string destination { get; set; }
+        public int maxSeats { get; set; }
+        public int passengers { get; set; }
+        public Customer[] customers { get; set; }
+
+        
+
 
         public Flight(int flightNumber, string org, string dest, int maxSeat)
         {
@@ -22,81 +26,18 @@ namespace AirLine
             this.destination = dest;
             this.maxSeats = maxSeat;
             this.passengers = 0;
+            customers= new Customer[maxSeats];
+
         }
 
-        public int getPassengers() { return passengers; }
-        public int getFlightNum() { return flightNum; }
+        
 
-        public int getMaxSeats() { return maxSeats; } // N
-
-        //CHANGE WHILE MAKING BOOKINGS
-        public void addPassenger(int customerId, string firstName, string lastName, string email) {
-            Array.Resize(ref customers, customers.Length + 1);
-            customers[customers.Length - 1] = new Customer(customerId, firstName, lastName,  email);
-            passengers++; 
-        }
-        //CHANGE WHILE MAKING BOOKINGS
-        public void removePassenger(int customerID)
+        
+        public override string ToString()
         {
-            int indexToRemove = -1;
-            for (int i = 0; i < customers.Length; i++)
-            {
-                if (customers[i].GetCustomerId() == customerID) 
-                {
-                    indexToRemove = i;
-                    break;
-                }
-            }
-
-            if (indexToRemove == -1)
-            {
-                Console.WriteLine("Customer not found.");
-                return;
-            }
-
-            Customer[] newCustomers = new Customer[customers.Length - 1];
-
-            for (int i = 0, j = 0; i < customers.Length; i++)
-            {
-                if (i != indexToRemove)
-                {
-                    newCustomers[j++] = customers[i];
-                }
-            }
-
-            customers = newCustomers;
-            passengers--;
-        }
-
-        public string getDetails()
-        {
-            string s = $"Flight Number: {flightNum}\n";
-            s += $"Origin: {origin}\n";
-            s += $"Destination: {destination}\n";
-            s += "Passengers on this flight:\n";
-
-            if (customers != null && customers.Any())
-            {
-                foreach (var customer in customers)
-                {
-                    s += $"- {customer.GetFirstName() +" " + customer.GetLastName()} (ID: {customer.GetCustomerId()})\n";
-                }
-            }
-            else
-            {
-                s += "No passengers on this flight.\n";
-            }
-
+            string s = "--Flight Details--";
+            s += "\nFlight Number: " + flightNum + "\nOrigin: " + origin + "\nDestination: " + destination + "\nMax Seats: " + maxSeats + "\nNo. of Passengers: " + passengers;
             return s;
         }
-
-        public string getSummary()
-        {
-            string s = $"Flight Number: {flightNum}\n";
-            s += $"Origin: {origin}\n";
-            s += $"Destination: {destination}\n";
-            return s;
-        }
-
     }
 }
